3d1a00f47724fec0f4337e9166d6e0464920890d
vicmaburrito <vmaldaraca@gmail.com>
2022-01-04 19:53:12 -0600

    Create Calculator file and display string from file js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Calculator from './components/Calculator';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = () => <div><h1>Lorem</h1></div>;
+const App = () => <div><h1><Calculator /></h1></div>;
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div> Hi From Calculator </div>
+    );
+  }
+}
+
+export default Calculator;
