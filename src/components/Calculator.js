306f680c778b5a992cbdec6c64f5cce1e2159d33
vicmaburrito <vmaldaraca@gmail.com>
2022-01-05 17:40:43 -0600

    Add event in buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -6,38 +7,43 @@ class Calculator extends React.Component {
     this.state = {};
   }
 
+  onClickHandler = (e) => {
+    const result = calculate(this.state, e.target.innerHTML);
+    this.setState(result);
+  }
+
   render() {
     return (
 
       <div className="calculator card">
 
-        <input type="text" className="calculator-screen z-depth-1" placeholder="0" disabled />
+        <input type="text" className="calculator-screen z-depth-1 " placeholder="0" disabled />
 
         <div className="calculator-keys">
 
-          <button type="button" className="operator btn btn-gray border-secondary">+/-</button>
-          <button type="button" className="operator btn btn-gray border-secondary">AC</button>
-          <button type="button" className="operator btn btn-gray border-secondary">%</button>
-          <button type="button" className="operator btn btn-Orange border-secondary">&divide;</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-gray border-secondary">+/-</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-gray border-secondary">AC</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-gray border-secondary">%</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-Orange border-secondary">&divide;</button>
 
-          <button type="button" className="btn btn-gray waves-effect border-secondary">7</button>
-          <button type="button" className="btn btn-gray waves-effect border-secondary">8</button>
-          <button type="button" className="btn btn-gray waves-effect border-secondary">9</button>
-          <button type="button" className="operator btn btn-Orange border-secondary">+</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">7</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">8</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">9</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-Orange border-secondary">+</button>
 
-          <button type="button" className="btn btn-gray waves-effect border-secondary">4</button>
-          <button type="button" className="btn btn-gray waves-effect border-secondary">5</button>
-          <button type="button" className="btn btn-gray waves-effect border-secondary">6</button>
-          <button type="button" className="operator btn btn-Orange border-secondary">-</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">4</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">5</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">6</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-Orange border-secondary">-</button>
 
-          <button type="button" className="btn btn-gray waves-effect border-secondary">1</button>
-          <button type="button" className="btn btn-gray waves-effect border-secondary">2</button>
-          <button type="button" className="btn btn-gray waves-effect border-secondary">3</button>
-          <button type="button" className="operator btn btn-Orange border-secondary">&times;</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">1</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">2</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray waves-effect border-secondary">3</button>
+          <button type="button" onClick={this.onClickHandler} className="operator btn btn-Orange border-secondary">&times;</button>
 
-          <button type="button" className="btn btn-gray zero border-secondary">0</button>
-          <button type="button" className="decimal function btn btn-gray dot border-secondary">.</button>
-          <button type="button" className="equal-sign operator btn btn-Orange equal border-secondary">=</button>
+          <button type="button" onClick={this.onClickHandler} className="btn btn-gray zero border-secondary">0</button>
+          <button type="button" onClick={this.onClickHandler} className="decimal function btn btn-gray dot border-secondary">.</button>
+          <button type="button" onClick={this.onClickHandler} className="equal-sign operator btn btn-Orange equal border-secondary">=</button>
 
         </div>
       </div>
