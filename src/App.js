a5bab2c51c5b0b20a8472e75f9ccbfd498322243
vicmaburrito <vmaldaraca@gmail.com>
2022-01-13 16:41:50 -0600

    Create navbar component and display it on page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,9 +12,12 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <>
+        <Navbar />
+        <div className="App">
+          <Calculator />
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-light justify-content-between">
+      <h1 className="title-math">Math Magicians</h1>
+    </nav>
+  );
+}
+export default Navbar;
