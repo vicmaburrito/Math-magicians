dbfb403919bf469f389c6bb04893b85ace014c5d
vicmaburrito <vmaldaraca@gmail.com>
2022-01-13 19:49:48 -0600

    Fix Quote component layout and complete milestone

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Quote.css';
 
 function Quote() {
   return (
-    <div className="m-5">
+    <div className="d-flex justify-content-center m-5">
       <div className="m-5 paragraph">
         <p>
           Mathematics is not about numbers, equations, computations or algorithms it is
