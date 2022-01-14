2be022f83699f744c6c6ebac1e25ef775a3e62a7
vicmaburrito <vmaldaraca@gmail.com>
2022-01-13 18:33:46 -0600

    Set Style in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="p-2 navbar d-flex justify-content-between navbar-light">
-      <h1 className="navbar-brand">Math Magicians</h1>
+      <span className="title-math">Math Magicians</span>
       <div className="menu-items">
-        <Link to="/math-magicians/Calculator">Calculator</Link>
+        <Link to="/math-magicians/" className="linkCalculator text-decoration-none">Home</Link>
+        <Link to="/math-magicians/Calculator" className="linkCalculator text-decoration-none">Calculator</Link>
       </div>
     </nav>
   );
